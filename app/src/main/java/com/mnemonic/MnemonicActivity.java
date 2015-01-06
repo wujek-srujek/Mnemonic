1ef0ff4ca943ff306cff74a1076c26d41bddc7b5
wujek srujek <wujek.srujek@gmail.com>
2015-01-06 22:33:21 +0100

    Integrates a file chooser

--- app/src/main/java/com/mnemonic/MnemonicActivity.java
+++ app/src/main/java/com/mnemonic/MnemonicActivity.java
@@ -2,19 +2,29 @@ package com.mnemonic;
 
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toolbar;
+
+import com.nononsenseapps.filepicker.FilePickerActivity;
+
+import java.io.File;
 
 
 public class MnemonicActivity extends Activity {
+
+    private final static String LAST_FILE_PATH_KEY = "lastFile";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_mnemonic);
-
         setActionBar((Toolbar) findViewById(R.id.toolbar));
     }
 
@@ -23,5 +33,48 @@ public class MnemonicActivity extends Activity {
         getMenuInflater().inflate(R.menu.menu_mnemonic, menu);
 
         return true;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK) {
+            String filePath = data.getData().getPath();
+            SharedPreferences.Editor editor = getPreferences(Context.MODE_PRIVATE).edit();
+            editor.putString(LAST_FILE_PATH_KEY, filePath);
+            editor.apply();
+
+            initializeTests(filePath);
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        String filePath = getPreferences(Context.MODE_PRIVATE).getString(LAST_FILE_PATH_KEY, null);
+        if (filePath != null) {
+            initializeTests(filePath);
+        }
+    }
+
+    public void chooseDirectory(MenuItem item) {
+        String startPath = getPreferences(Context.MODE_PRIVATE).getString(LAST_FILE_PATH_KEY,
+                Environment.getExternalStorageDirectory().getAbsolutePath());
+        File startFile = new File(startPath);
+        if (startFile.isFile()) {
+            startPath = startFile.getParent();
+        }
+
+        Intent intent = new Intent(this, FilePickerActivity.class);
+        intent.putExtra(FilePickerActivity.EXTRA_MODE, FilePickerActivity.MODE_FILE);
+        intent.putExtra(FilePickerActivity.EXTRA_ALLOW_MULTIPLE, false);
+        intent.putExtra(FilePickerActivity.EXTRA_START_PATH, startPath);
+
+        startActivityForResult(intent, 0);
+    }
+
+    private void initializeTests(String filePath) {
     }
 }
