9dabdfd147604195104911f4a91c53c280ba8267
wujek srujek <wujek.srujek@gmail.com>
2015-01-11 09:51:22 +0100

    Replaces ugly swiping with ViewPager

---
+++ app/src/main/java/com/mnemonic/TaskPagerAdapter.java
@@ -0,0 +1,60 @@
+package com.mnemonic;
+
+
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+
+public class TaskPagerAdapter extends PagerAdapter {
+
+    private LayoutInflater inflater;
+
+    private List<Task> tasks;
+
+    public TaskPagerAdapter(LayoutInflater inflater, List<Task> tasks) {
+        this.inflater = inflater;
+        this.tasks = tasks;
+    }
+
+    @Override
+    public int getCount() {
+        // question and answer are separate pages
+        return tasks.size() * 2;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        Task task = tasks.get(taskNumberForPosition(position));
+
+        TextView textView = (TextView) inflater.inflate(R.layout.task_page, container, false);
+        textView.setText(isQuestion(position) ? task.getQuestion() : task.getAnswer());
+        container.addView(textView);
+
+        return textView;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        TextView textView = (TextView) object;
+        textView.setText(null);
+        container.removeView(textView);
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    public int taskNumberForPosition(int position) {
+        return position / 2;
+    }
+
+    public boolean isQuestion(int position) {
+        return position % 2 == 0;
+    }
+}
--- app/src/main/java/com/mnemonic/TestActivity.java
+++ app/src/main/java/com/mnemonic/TestActivity.java
@@ -4,8 +4,7 @@ package com.mnemonic;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.view.View;
-import android.widget.TextView;
+import android.support.v4.view.ViewPager;
 import android.widget.Toolbar;
 
 import java.util.List;
@@ -19,15 +18,13 @@ public class TestActivity extends Activity {
 
     private final static String IS_QUESTION_BUNDLE_KEY = "isQuestion";
 
-    private TextView taskTextView;
-
-    private Test test;
-
     private List<Task> tasks;
 
     private int currentTaskNumber;
 
     private boolean isQuestion;
+
+    private TaskPagerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,9 +33,7 @@ public class TestActivity extends Activity {
         setContentView(R.layout.activity_test);
         setActionBar((Toolbar) findViewById(R.id.toolbar));
 
-        taskTextView = (TextView) findViewById(R.id.task);
-
-        test = (Test) getIntent().getSerializableExtra(TEST_EXTRA);
+        Test test = (Test) getIntent().getSerializableExtra(TEST_EXTRA);
         tasks = new TaskParser().parse(test.getFile(), test.getFirstLineNumber(), test.getLastLineNumber());
 
         getActionBar().setDisplayHomeAsUpEnabled(true);
@@ -55,26 +50,30 @@ public class TestActivity extends Activity {
         }
 
         if (!tasks.isEmpty()) {
-            findViewById(R.id.task).setOnClickListener(new View.OnClickListener() {
+            ViewPager pager = (ViewPager) findViewById(R.id.pager);
+            adapter = new TaskPagerAdapter(getLayoutInflater(), tasks);
+            pager.setAdapter(adapter);
+            pager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
 
                 @Override
-                public void onClick(View v) {
-                    if (isQuestion) {
-                        // question always has the next step - the answer
-                        isQuestion = false;
-                    } else if (currentTaskNumber < tasks.size() - 1) {
-                        // answer shown, but there are still more tasks
-                        ++currentTaskNumber;
-                        isQuestion = true;
-                    } else {
-                        // answer of the last task shown, cycle to the beginning
-                        currentTaskNumber = 0;
-                        isQuestion = true;
-                    }
+                public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                    // nope
+                }
+
+                @Override
+                public void onPageSelected(int position) {
+                    isQuestion = adapter.isQuestion(position);
+                    currentTaskNumber = adapter.taskNumberForPosition(position);
 
                     updateViews();
                 }
+
+                @Override
+                public void onPageScrollStateChanged(int state) {
+                    // nope
+                }
             });
+
             updateViews();
         }
     }
@@ -88,19 +87,8 @@ public class TestActivity extends Activity {
     }
 
     private void updateViews() {
-        Task task = tasks.get(currentTaskNumber);
-        String taskPartIdentifier;
-        String taskPartText;
-        if (isQuestion) {
-            taskPartIdentifier = getString(R.string.question);
-            taskPartText = task.getQuestion();
-        } else {
-            taskPartIdentifier = getString(R.string.answer);
-            taskPartText = task.getAnswer();
-        }
-        String taskInfo = String.format(getString(R.string.task_info_format), taskPartIdentifier, (currentTaskNumber) + 1, tasks.size());
-
+        String taskPart = getString(isQuestion ? R.string.question : R.string.answer);
+        String taskInfo = String.format(getString(R.string.task_info_format), taskPart, (currentTaskNumber) + 1, tasks.size());
         getActionBar().setSubtitle(taskInfo);
-        taskTextView.setText(taskPartText);
     }
 }
